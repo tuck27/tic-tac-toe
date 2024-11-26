6f5c266709b23bc9e8e01ff8bb7827e427173496
Maya <mayas2744@tuckahoeschools.org>
2024-11-26 13:31:32 +0000

    third commit

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const TicTac = {
-    cPlayer: "X",
-    state: Array(9).fill(null),
-    gameOver: "false",
+let board = ["", "", "", "", "", "", "", ""];
+let currentPlayer = X;
+let gameOver = false;
 
-    init() {
-        this.cBoard();
-        document.getElementbyId("reset")
-        document.addEventListener("click", () => this.reset());
-    },
-
-    cBoard() {
-        const board = document.getElementById("board");
-        board.innerHTML = "";
-        this.state.forEach((_, i) => {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.dataset.index = i;
-            board.appendChild(cell);
-        })
-    }
-};
